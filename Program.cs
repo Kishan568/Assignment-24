7ccac66ae108af214f8d23ce982836a21e498eb8
kishan.kumar4@mphasis.com <kishan.kumar4@mphasis.com>
2023-08-07 15:42:00 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Assignment24;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()
+            {
+                Id = 07,
+                FirstName = "Kishan",
+                LastName = "Kumar",
+                Salary = 72000.00
+            };
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("E:\\Users\\kishan\\source\\repos\\Day 25\\Assignment24\\Employee.cs", FileMode.Create))
+            {
+                formatter.Serialize(fs, employee);
+                Console.WriteLine("Created and Serialized");
+                Console.WriteLine(employee.Id);
+                Console.WriteLine(employee.FirstName);
+                Console.WriteLine(employee.LastName);
+                Console.WriteLine(employee.Salary);
+
+            }
+            using (FileStream fs = new FileStream("E:\\Users\\kishan\\source\\repos\\Day 25\\Assignment24\\Employee.cs", FileMode.Open))
+            {
+                employee = (Employee)formatter.Deserialize(fs);
+                Console.WriteLine("De-Serialized");
+                Console.WriteLine(employee.Id);
+                Console.WriteLine(employee.FirstName);
+                Console.WriteLine(employee.LastName);
+                Console.WriteLine(employee.Salary);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
